92c9141c7ad526b86192c0428d68f9a7037cf531
Ace LaCabe <150067821+AceChianni@users.noreply.github.com>
2024-12-04 15:44:33 +0000

    edit json to run dev

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 // middleware for json POST body
 app.use(express.json()); 
 
+// Root Route (GET)
 app.get("/", (req, res) => {
     res.send("hello");
 })
@@ -14,3 +15,8 @@ app.post("/data", (req, res) => {
     console.log(data);
     res.status(200).send(data)
 })
+
+// Start the Server
+app.listen(3000, () => {
+    console.log("started server!!! on port 3000")
+  })
